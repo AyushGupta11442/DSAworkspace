f4ede2236ef83b24b9529a11b2814f72e18586f5
AyushGupta11442 <ayushgupta11442@gmail.com>
2026-01-13 16:56:29 +0530

    basic addded

---
+++ templates/c.c
@@ -0,0 +1,29 @@
+Problem:
+Site:
+Link:
+Difficulty:
+
+
+Companies:
+Topics:
+
+
+Approach 1 (Brute Force):
+- Idea:
+- Time:
+- Space:
+
+
+Approach 2 (Better):
+- Idea:
+- Time:
+- Space:
+
+
+Approach 3 (Optimal):
+- Idea:
+- Time:
+- Space:
+
+
+Notes:
